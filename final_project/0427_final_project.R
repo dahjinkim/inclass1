f403593d2be869d6011a30316515b536f76b1ac6
oadenekan123 <42779218+oadenekan123@users.noreply.github.com>
2020-04-27 22:49:24 -0400

    covid tracking and notes on needed data

--- final_project/0427_final_project.R
+++ final_project/0427_final_project.R
@@ -1,2 +1,8 @@
 # read in the data
-data = read.csv('C:/Users/oyina/src/senior_2019-2020/spring_2020/political_data_science/group_work/final_project/daily.csv')
+covid_tracking = read.csv('C:/Users/oyina/src/senior_2019-2020/spring_2020/political_data_science/group_work/final_project/daily.csv')
+# write web scrapper or use api for 
+
+# contributions from individuals
+# contributions from committess and to candidates and independent expenditures
+
+# https://www.fec.gov/data/browse-data/?tab=bulk-data
