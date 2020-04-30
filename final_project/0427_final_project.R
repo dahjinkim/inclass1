f88949a4a3eeb96e5c0f38b35684830cf02d208b
oadenekan123 <42779218+oadenekan123@users.noreply.github.com>
2020-04-30 15:37:36 -0400

    date things

--- final_project/0427_final_project.R
+++ final_project/0427_final_project.R
@@ -17,14 +17,60 @@ contrib = contrib_og %>%
   mutate(year = str_sub(date, -4, -1)) %>%
   mutate(day = str_sub(date, -6, -5)) %>%
   mutate(month = str_sub(date, -8, -7)) %>%
-  filter(year != "2018")
-  # mutate(date = as.Date(date, "%m/%d/%Y"))
+  filter(year != "2018") %>%
+  mutate(month = paste0("0", month)) %>%
+  mutate(month = str_sub(month, -2, -1)) %>%
+  mutate(date_ = as.Date(paste0(year, "-", month, "-", day))) #maybe?
+  # mutate(date_ = as.Date(date, "%m/%d/%Y"))
 
 
 covid = covid_og %>%
   mutate(year = str_sub(date, 1, 4)) %>%
   mutate(day = str_sub(date, 7, 8)) %>%
-  mutate(month = str_sub(date, 5, 6))
+  mutate(month = str_sub(date, 5, 6)) %>%
+  mutate(date_ = as.Date(paste0(year, "-", month, "-", day))) #maybe?
+
+
+# create date range vector
+start_date = as.Date("2019-01-01")
+date_ranges = rep(NA, 106)
+for (i in 1:106) {
+  date_ranges[i] = as.character(as.Date(start_date))
+  start_date = start_date + 7
+}
+date_ranges = as.Date(date_ranges)
+
+
+# add row to covid data for week
+covid_week = rep(NA, nrow(covid))
+for (row in 1:nrow(covid)) {
+  for (date in 1:length(date_ranges)) {
+    if (covid$date_[row] <= date_ranges[date]) {
+      # print(covid$date[row])
+      # print(date_ranges[date])
+      covid_week[row] = as.character(as.Date(date_ranges[date]))
+      break
+    }
+  }
+}
+covid = covid %>%
+  mutate(week = covid_week)
+
+# add row to contrib data for week
+contrib_week = rep(NA, nrow(contrib))
+for (row in 1:nrow(contrib)) {
+  for (date in 1:length(date_ranges)) {
+    if (contrib$date_[row] <= date_ranges[date]) {
+      # print(covid$date[row])
+      # print(date_ranges[date])
+      contrib_week[row] = as.character(as.Date(date_ranges[date]))
+      break
+    }
+  }
+}
+print(contrib_week)
+contrib = contrib %>%
+  mutate(week = contrib_week)
 
 
 
