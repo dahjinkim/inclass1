ba49575554c863588a6e92dc70f00b763ca4e353
Jin <60522038+dahjinkim@users.noreply.github.com>
2020-05-04 14:54:19 -0500

    Create plots.R

---
+++ final_project/plots.R
@@ -0,0 +1,54 @@
+## Plots
+
+rm(list=ls())
+
+#read packages
+library(readr)
+library(dplyr)
+library(stringr)
+library(ggplot2)
+
+
+#read data
+fullweekly <- read.csv("fullweekly.csv")
+fullweekly$week <- as.Date(fullweekly$week)
+
+
+#full trend
+full <- ggplot(data=fullweekly)+
+  geom_point(mapping= aes(x=week, y=log(deathsum)))+
+  geom_smooth(mapping = aes(x=week, y=log(deathsum)), method=loess) +
+  geom_point(mapping=aes(x=week, y=log(committee), color="Committee"), alpha=0.5)+
+  geom_smooth(mapping=aes(x=week, y=log(committee), color="Committee"), method=loess) +
+  geom_point(mapping=aes(x=week, y=log(individual), color="Individual"), alpha=0.5)+
+  geom_smooth(mapping=aes(x=week, y=log(individual), color="Individual"), method=loess) +
+  scale_x_date(limits=c(as.Date("2020-01-01"),as.Date("2020-4-28" )))
+full +  labs(title="Full Trend", y="Logged Value") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  scale_color_manual(name = "Contribution", values = c("Committee" = "darkgreen", "Individual" = "darkorange"), labels = c("Committee", "Individual"))
+
+
+# all states
+allstate <- ggplot(data=fullweekly)+
+  geom_line(mapping= aes(x=week, y=log(deathsum)))+
+  geom_line(mapping=aes(x=week, y=log(committee), color="Committee"))+
+  geom_line(mapping=aes(x=week, y=log(individual), color="Individual"))+
+  scale_x_date(limits=c(as.Date("2020-1-01"),as.Date("2020-4-28" )))+
+  facet_wrap(~state)
+allstate +  labs(title="Contribution and COVID19 Deaths (50 States)", y="Logged Value") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  scale_color_manual(name = "Contribution", values = c("Committee" = "darkgreen", "Individual" = "darkorange"), labels = c("Committee", "Individual"))
+
+
+# individual state (eg. NY)
+state1 <- ggplot(data=filter(fullweekly, state=="NY"))+
+  geom_line(mapping= aes(x=week, y=log(deathsum)))+
+  geom_line(mapping=aes(x=week, y=log(committee), color="Committee"))+
+  geom_line(mapping=aes(x=week, y=log(individual), color="Individual"))+
+  scale_x_date(limits=c(as.Date("2020-1-01"),as.Date("2020-4-28" )))
+state1 +  labs(title="Contribution and COVID19 Deaths (New York)", y="Logged Value") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  scale_color_manual(name = "Contribution", values = c("Committee" = "darkgreen", "Individual" = "darkorange"), labels = c("Committee", "Individual"))
+
+
+# we can also make an option to choose which contribution to see if we have time
