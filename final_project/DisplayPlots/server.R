e75043a0a3d3ff9f3b7ff241882294a9a0d993c0
dahjinkim <60522038+dahjinkim@users.noreply.github.com>
2020-05-04 20:31:40 -0500

    Update server.R

--- final_project/DisplayPlots/server.R
+++ final_project/DisplayPlots/server.R
@@ -10,36 +10,42 @@ library(readr)
 library(dplyr)
 library(ggplot2)
 # setwd("~/GitHub/jmontgome/ry.github.io/PDS/Datasets/SenateForecast")
-working_dir = "C:/Users/oyina/src/senior_2019-2020/spring_2020/political_data_science/group_work/final_project/"
-weeklyData<-read.csv(paste0(working_dir, "/fullweekly.csv")) 
-
+# working_dir = "C:/Users/oyina/src/senior_2019-2020/spring_2020/political_data_science/group_work/final_project/"
+# weeklyData<-read.csv(paste0(working_dir, "/fullweekly.csv")) 
+# weeklyData<-read.csv("fullweekly.csv")
 
 library(shiny)
 shinyServer(function(input, output) {
     ## This will just be executed once
-    working_dir = "C:/Users/oyina/src/senior_2019-2020/spring_2020/political_data_science/group_work/final_project/"
-    weeklyData<-read.csv(paste0(working_dir, "/fullweekly.csv")) 
-    # This will be updated when options are changed.
+    # working_dir = "C:/Users/User/Documents/GitHub/inclass1/final_project/"
+    weeklyData<-read.csv("../fullweekly.csv") 
+    #weeklyData<-read.csv("C:/Users/User/Documents/GitHub/inclass1/final_project/fullweekly.csv")
+  # This will be updated when options are changed.
     passData<-reactive({ 
         WeeklyForState<-weeklyData %>%
-            filter(state==input$stateSelect) %>%
+            filter(state == input$stateSelect) %>%
+            mutate(week = as.Date(week))
             # select(c("Poll", "daysLeft", "Candidateidentifier")) %>% 
             # group_by(Candidateidentifier)
+        print(input$stateSelect)
         return(WeeklyForState)
     })
     # Now we prepare the output
     output$trendPlot <-renderPlot({ # Note the brace
         # print("this")
-        thePlot<-(ggplot(passData()) +  # Note passData() is a function
+        thePlot<-ggplot(passData()) +  # Note passData() is a function
             geom_line(mapping= aes(x=week, y=log(deathsum))) +
             geom_line(mapping=aes(x=week, y=log(committee), color="Committee"))+
             geom_line(mapping=aes(x=week, y=log(individual), color="Individual"))+
             scale_x_date(limits=c(as.Date("2020-1-01"),as.Date("2020-4-28" ))) +
-            labs(title="Contribution and COVID19 Deaths (New York)", y="Logged Value") +
+            labs(title=paste("Contribution and COVID19 Deaths in", input$stateSelect), y="Logged Value", x="Month") +
             theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-            scale_color_manual(name = "Contribution", values = c("Committee" = "darkgreen", "Individual" = "darkorange"), labels = c("Committee", "Individual")))
+            scale_color_manual(name = "Contribution", values = c("Committee" = "darkgreen", "Individual" = "darkorange"), labels = c("Committee", "Individual"))
             print(thePlot)
             return(thePlot)
     })
 }) # Close function/shinyServer call
 
+# weeklyData1$week <- as.Date(weeklyData1$week)
+# class(weeklyData1$week)
+# dim(weeklyData1)
